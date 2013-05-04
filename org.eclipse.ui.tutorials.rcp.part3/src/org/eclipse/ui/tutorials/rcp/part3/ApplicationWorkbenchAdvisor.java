679bd87309c8228fc96ee9ac4a624fbc739a2e9c
haiming.wang <haiming.wang@dragonflow.com>
2013-05-04 21:16:26 +0800

    每 一次上传项目

---
+++ org.eclipse.ui.tutorials.rcp.part3/src/org/eclipse/ui/tutorials/rcp/part3/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,28 @@
+package org.eclipse.ui.tutorials.rcp.part3;
+
+import org.eclipse.ui.application.IWorkbenchConfigurer;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+/**
+ * This workbench advisor creates the window advisor, and specifies
+ * the perspective id for the initial window.
+ */
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+	
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return Perspective.ID;
+	} 
+	@Override
+	public void initialize(IWorkbenchConfigurer configurer) {
+		// TODO Auto-generated method stub
+		configurer.setSaveAndRestore(true);
+		super.initialize(configurer);
+	}
+	
+}
